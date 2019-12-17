b88c90fe759e26a752646e457ec01b563d99120c
Toby Clemson <tobyclemson@gmail.com>
2019-12-17 10:05:47 -0500

    Add optional eftest command.

---
+++ lib/ruby_leiningen/commands/plugins/eftest.rb
@@ -0,0 +1,27 @@
+require_relative '../../commands'
+
+RubyLeiningen::Commands.define_custom_command("eftest") do |config, opts|
+  only = opts[:only] ? [":only #{opts[:only]}"] : []
+  specific = (opts[:test_selectors] || []).map { |m| ":#{m}" }
+  all = [":all"]
+
+  test_selectors =
+    if only.any?
+      only
+    elsif specific.any?
+      specific
+    else
+      all
+    end
+
+  namespaces = opts[:namespaces] || []
+  files = opts[:files] || []
+
+  arguments = namespaces.concat(files).concat(test_selectors)
+
+  config.on_command_builder do |command|
+    arguments.inject(command) do |c, arg|
+      c.with_argument(arg)
+    end
+  end
+end
---
+++ spec/ruby_leiningen/commands/plugins/eftest_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+require_relative '../../../../lib/ruby_leiningen/commands/plugins/eftest'
+
+describe RubyLeiningen::Commands::Eftest do
+  it 'calls the lein eftest subcommand' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('lein eftest :all', any_args))
+
+    command.execute
+  end
+
+  it 'passes the provided test selectors' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('lein eftest :unit :integration', any_args))
+
+    command.execute(test_selectors: [:unit, :integration])
+  end
+
+  it 'passes the provided namespaces' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with('lein eftest mylib.core mylib.helpers :all', any_args))
+
+    command.execute(namespaces: ["mylib.core", "mylib.helpers"])
+  end
+
+  it 'passes the provided files' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                'lein eftest src/mylib/core.clj src/mylib/helpers.clj :all',
+                any_args))
+
+    command.execute(files: ["src/mylib/core.clj", "src/mylib/helpers.clj"])
+  end
+
+  it 'allows test selectors and namespaces' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                'lein eftest mylib.core mylib.helpers :unit :integration',
+                any_args))
+
+    command.execute(
+        namespaces: ["mylib.core", "mylib.helpers"],
+        test_selectors: [:unit, :integration])
+  end
+
+  it 'allows the only test selector' do
+    command = RubyLeiningen::Commands::Eftest.new(binary: 'lein')
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                'lein eftest :only some.namespace/test-something',
+                any_args))
+
+    command.execute(
+        only: "some.namespace/test-something")
+  end
+end
